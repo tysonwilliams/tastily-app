a0136ca95f5275cdd559f4aff6c87960e0b213db
Tyson Williams <tyson.j.williams@gmail.com>
2019-07-22 16:16:05 -0600

    Add Pitch page component

---
+++ src/components/PitchPage/PitchPage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #10c7cd;
+`;
+
+const Subtitle = styled.h2`
+  color: black;
+`;
+
+const Tastily = styled.span`
+  color: #10c7cd;
+`;
+
+const AlgoHealth = styled.span`
+  color: black;
+`;
+
+function PitchPage() {
+  return (
+    <div className="App">
+      <Title>
+        Tastily / <AlgoHealth>Algo Health</AlgoHealth>
+      </Title>
+      <Subtitle>
+        Welcome to the future site of <Tastily>Tastily</Tastily>, the{" "}
+        <span style={{ textDecoration: "underline" }}>AI based</span> nutrition
+        management app!
+      </Subtitle>
+      <h2>
+        <a
+          href="https://appw.pitchdeck.io/docs/?plKFAu"
+          style={{ textDecorationColor: "#10c7cd", color: "black" }}
+        >
+          The Pitch Deck
+        </a>
+      </h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<PitchPage />, rootElement);
