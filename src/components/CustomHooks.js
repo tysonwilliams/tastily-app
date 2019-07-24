5c7db72fcdd8c4d4655d36e8966e19da4e1c944a
Tyson Williams <tyson.j.williams@gmail.com>
2019-07-24 13:39:59 -0600

    Add signup form custom hook to Signup Form component
    Also reformat custom hook

--- src/components/CustomHooks.js
+++ src/components/CustomHooks.js
@@ -1,25 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const useSignUpForm = (callback) => {
+const useSignUpForm = callback => {
   const [inputs, setInputs] = useState({});
 
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     if (event) {
       event.preventDefault();
     }
     callback();
-  }
+  };
 
-  const handleInputChange = (event) => {
+  const handleInputChange = event => {
     event.persist();
-    setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
-  }
+    setInputs(inputs => ({
+      ...inputs,
+      [event.target.name]: event.target.value
+    }));
+  };
 
   return {
     handleSubmit,
     handleInputChange,
     inputs
-  }
-}
+  };
+};
 
 export default useSignUpForm;
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,19 +1,47 @@
 import React from "react";
 
+import useSignUpForm from "./CustomHooks.js";
+
 function SignupForm() {
+  const signup = () => {
+    alert(`User Created!
+           Name: ${inputs.firstName} ${inputs.lastName}
+           Email: ${inputs.email}`);
+  };
+
+  const { inputs, handleInputChange, handleSubmit } = useSignUpForm(signup);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>First name:</label>
-        <input type="text" name="firstName" required />
+        <input
+          type="text"
+          name="firstName"
+          onChange={handleInputChange}
+          value={inputs.firstName}
+          required
+        />
       </div>
       <div>
         <label>Last name:</label>
-        <input type="text" name="lastName" required />
+        <input
+          type="text"
+          name="lastName"
+          onChange={handleInputChange}
+          value={inputs.lastName}
+          required
+        />
       </div>
       <div>
         <label>Email:</label>
-        <input type="email" name="email" required />
+        <input
+          type="email"
+          name="email"
+          onChange={handleInputChange}
+          value={inputs.email}
+          required
+        />
       </div>
       <button type="submit">Sign Up</button>
     </form>
